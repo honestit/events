9294071cb698527817e4ca898e9c758921a4c5f9
honestit <michal.kupisinski@gmail.com>
2020-12-05 16:37:01 +0100

    #2 ukm  :: powiązanie użytkownika z wydarzeniem
    Uzupełniona informacja o użytkowniku związanym z wydarzeniem

--- src/main/java/honestit/projects/events/model/event/SubscribedEvent.java
+++ src/main/java/honestit/projects/events/model/event/SubscribedEvent.java
@@ -1,16 +1,27 @@
 package honestit.projects.events.model.event;
 
+import honestit.projects.events.model.user.User;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "user_subscribed_events")
 @Data @NoArgsConstructor @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true, of = "subscribedOn") @ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true, of = {"subscribedOn", "userId"})
+@ToString(callSuper = true, exclude = "user")
 public class SubscribedEvent extends Event {
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(insertable = false, updatable = false)
+    private Long userId;
     private LocalDateTime subscribedOn;
+
+    void setUser(User user) {
+        this.user = user;
+        this.userId = user.getId();
+    }
 }
