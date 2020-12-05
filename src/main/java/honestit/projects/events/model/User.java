cc6ca43e41b94a5a7753e94f0a5456809c416636
honestit <michal.kupisinski@gmail.com>
2020-12-05 12:55:58 +0100

    #2 ukm  :: dodano reprezentację użytkownika

---
+++ src/main/java/honestit/projects/events/model/User.java
@@ -0,0 +1,25 @@
+package honestit.projects.events.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
