45651dfbd2dd91c4048d874751828ad0309438d2
honestit <michal.kupisinski@gmail.com>
2020-12-06 12:10:47 +0100

    #5 rejestracja :: Poprawiony brak komponentu na konwerterze

--- src/main/java/honestit/projects/eventuator/model/user/UserConverter.java
+++ src/main/java/honestit/projects/eventuator/model/user/UserConverter.java
@@ -1,7 +1,9 @@
 package honestit.projects.eventuator.model.user;
 
 import honestit.projects.eventuator.accounts.registration.internal.InternalRegistrationRequest;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserConverter {
 
     public User from(InternalRegistrationRequest request) {
