867c4c9a03eb2dd8e1e916de4c4fdfd61f901d9a
honestit <michal.kupisinski@gmail.com>
2020-12-12 12:15:48 +0100

    #11 rejestracja-2 :: obsługa aktywacji w warstwie serwisowej

--- src/main/java/honestit/projects/eventuator/accounts/access/DefaultAuthUser.java
+++ src/main/java/honestit/projects/eventuator/accounts/access/DefaultAuthUser.java
@@ -1,12 +1,19 @@
 package honestit.projects.eventuator.accounts.access;
 
+import honestit.projects.eventuator.model.user.User;
+import honestit.projects.eventuator.model.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j @RequiredArgsConstructor
 public class DefaultAuthUser implements AuthUser {
+
+    private final UserRepository userRepository;
 
     @Override
     public String getUsername() {
@@ -14,7 +21,19 @@ public class DefaultAuthUser implements AuthUser {
     }
 
     @Override
-    public boolean activate(String token) {
-        return false;
+    @Transactional
+    public boolean activate(String tokenValue) {
+        log.debug("Activating user with token: {}", tokenValue);
+        Optional<User> optionalUser = userRepository.getUserByActivationTokenValue(tokenValue);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.getCredentials().setActive(true);
+            user.setActivationToken(null);
+            log.debug("Activating user: {}", user);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
